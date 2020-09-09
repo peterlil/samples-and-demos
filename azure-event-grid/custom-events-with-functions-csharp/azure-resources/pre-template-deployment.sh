12818c7ccc2e70b5141ee41049f44cbd39244117
Peter Liljenroth <sail@live.se>
2020-09-09 15:02:31 +0200

    key vault deployment

--- azure-event-grid/custom-events-with-functions-csharp/azure-resources/pre-template-deployment.sh
+++ azure-event-grid/custom-events-with-functions-csharp/azure-resources/pre-template-deployment.sh
@@ -3,6 +3,18 @@ rgName=$1
 # Second argument is the location of the resource group
 location=$2
 
+################################################################################
+### Create the resource group
+################################################################################
 if [ $(az group exists --name $rgName) = false ]; then
     az group create --name $rgName --location $location
 fi
+
+################################################################################
+### Deploy Key Vault
+################################################################################
+
+# The name of the Key Vault and the ObjectID of the user should not be checked 
+# in to source control, hence they are not in the parameter file.
+
+echo $(pwd)
