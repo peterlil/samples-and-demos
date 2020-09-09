3323591a1bd2adb61e4cb389f77d33acb6bce050
Peter Liljenroth <sail@live.se>
2020-09-09 10:45:43 +0200

    added script for creating the resource group

---
+++ azure-event-grid/custom-events-with-functions-csharp/azure-resources/pre-template-deployment.sh
@@ -0,0 +1,10 @@
+﻿#!/bin/bash
+
+# First argument is the resource group name
+RESOURCEGROUPNAME = $1
+# Second argument is the location of the resource group
+LOCATION = $2
+
+if [ $(az group exists --name $RESOURCEGROUPNAME) = false ]; then
+    az group create --name $RESOURCEGROUPNAME --location $LOCATION
+fi
