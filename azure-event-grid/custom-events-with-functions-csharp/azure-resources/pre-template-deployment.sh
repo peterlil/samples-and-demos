c27bd45e387f4c9de182856f01f22ab9e1ab4212
Peter Liljenroth <sail@live.se>
2020-09-09 12:15:24 +0200

    troubleshoot yaml

--- azure-event-grid/custom-events-with-functions-csharp/azure-resources/pre-template-deployment.sh
+++ azure-event-grid/custom-events-with-functions-csharp/azure-resources/pre-template-deployment.sh
@@ -1,8 +1,11 @@
-﻿# First argument is the resource group name
-RESOURCEGROUPNAME=$1
-# Second argument is the location of the resource group
-LOCATION=$2
-
-if [ $(az group exists --name $RESOURCEGROUPNAME) = false ]; then
-    az group create --name $RESOURCEGROUPNAME --location $LOCATION
-fi
+﻿az --version
+echo $1
+echo $2
+## First argument is the resource group name
+#RESOURCEGROUPNAME=$1
+## Second argument is the location of the resource group
+#LOCATION=$2
+#
+#if [ $(az group exists --name $RESOURCEGROUPNAME) = false ]; then
+#    az group create --name $RESOURCEGROUPNAME --location $LOCATION
+#fi
