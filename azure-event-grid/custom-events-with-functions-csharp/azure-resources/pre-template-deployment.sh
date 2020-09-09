50900f75e571dbf5f6f8d4fb437fa6e99259056f
Peter Liljenroth <sail@live.se>
2020-09-09 15:17:02 +0200

    replacing values in parameter file

--- azure-event-grid/custom-events-with-functions-csharp/azure-resources/pre-template-deployment.sh
+++ azure-event-grid/custom-events-with-functions-csharp/azure-resources/pre-template-deployment.sh
@@ -1,7 +1,10 @@
-﻿# First argument is the resource group name
+﻿#
+# First argument is the resource group name
 rgName=$1
 # Second argument is the location of the resource group
 location=$2
+# Third argument is the name of the Key Vault
+keyVaultName=$3
 
 ################################################################################
 ### Create the resource group
@@ -16,5 +19,6 @@ fi
 
 # The name of the Key Vault and the ObjectID of the user should not be checked 
 # in to source control, hence they are not in the parameter file.
-
-echo $(pwd)
+#
+#echo $(pwd)
+sed -i 's/#keyvaultname#/$keyVaultName/' ./azure-event-grid/custom-events-with-functions-csharp/azure-resources/azuredeploy.keyvault.parameters.json
