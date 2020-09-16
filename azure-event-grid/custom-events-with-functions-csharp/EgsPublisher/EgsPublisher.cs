6e9625258f96987a87c1aebcb0d7f5865b4f745a
Peter Liljenroth <sail@live.se>
2020-09-16 15:51:53 +0200

    testing trace logging to AppInsights

--- azure-event-grid/custom-events-with-functions-csharp/EgsPublisher/EgsPublisher.cs
+++ azure-event-grid/custom-events-with-functions-csharp/EgsPublisher/EgsPublisher.cs
@@ -19,6 +19,8 @@ namespace Peterlabs.Samples.Function
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
+            log.LogTrace("Testing a trace message.");
+
             string name = req.Query["name"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
