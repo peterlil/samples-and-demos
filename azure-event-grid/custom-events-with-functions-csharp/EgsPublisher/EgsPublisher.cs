4a512a3acfedde64a4ff2987c6fced7e0efe4e4c
peterlil <peterlil@microsoft.com>
2020-10-27 14:58:04 +0100

    .

--- azure-event-grid/custom-events-with-functions-csharp/EgsPublisher/EgsPublisher.cs
+++ azure-event-grid/custom-events-with-functions-csharp/EgsPublisher/EgsPublisher.cs
@@ -8,15 +8,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
-
-using Microsoft.Azure.EventGrid.Models;
-
-// NuGet required: https://www.nuget.org/packages/Microsoft.Azure.EventGrid/
-// dotnet add package Microsoft.Azure.EventGrid --version 3.2.0
-
 namespace Peterlabs.Samples.Function
 {
-    
     public static class EgsPublisher
     {
         [FunctionName("EgsPublisher")]
@@ -24,23 +17,17 @@ namespace Peterlabs.Samples.Function
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function is preparing a custom Event Grid event.");
-            
-            string traceId = Guid.NewGuid().ToString();
-            var testEvent = new EventGridEvent()
-            {
-                Id = Guid.NewGuid().ToString(),
-                Subject = "My custom event",
-                EventType = "my-customer-event-type",
-                EventTime = DateTime.UtcNow,
-                Data = traceId,
-                DataVersion = "1.0"
-            };
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            
-            string responseMessage = "";
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
             return new OkObjectResult(responseMessage);
         }
---
+++ docs/devops/jfrog/private-nuget-repository.md
@@ -0,0 +1,33 @@
+﻿# Set up a private (and free) NuGet repository
+
+This article will show you a simple way of securely setting up a private NuGet feed over the Internet and at the time of writing, at no cost. 
+First of all, use Google Chrome are even the new Chromium Edge is not supported with JFrog.
+
+# Create the subscription
+
+Start by signing up for a [free cloud subscription at JFrog](https://jfrog.com/) using Google Chrome.
+
+![Start page of JFrog with an option to create a free cloud subscription.]./../../../img/jfrog/jfrog1.jpg)
+
+
+
+and make sure to select to host it in Azure.
+
+Activate by the e-mail sent
+
+Log in (still in Chrome)
+
+# Delete default repos
+Sign in to your subscription (https://<your subscription name>.jfrog.io) and navigate to Administration->Repositories. 
+Hover over the repositories and remove the ones that was created by default.
+
+# Add a new local repository
+Create a new local repository and select package type NuGet when you are prompted.
+When entering 'Repository Key' you should enter the name of your repository. It could be confusing that they are calling it key, when they actually mean name.
+
+# Remove default permissions
+
+Default permissions are:
+
+
+
