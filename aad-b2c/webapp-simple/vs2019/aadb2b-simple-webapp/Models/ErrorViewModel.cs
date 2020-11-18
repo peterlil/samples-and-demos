abddf127dfd3da9ad2a4724f4eb3d19f8a6df540
peterlil <peterlil@microsoft.com>
2020-11-18 17:29:53 +0100

    created and documented the aadb2c-simple-webapp

---
+++ aad-b2c/webapp-simple/vs2019/aadb2b-simple-webapp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace aadb2b_simple_webapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
