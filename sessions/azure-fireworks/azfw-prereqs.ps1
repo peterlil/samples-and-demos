287931ade3e3769a180e732b651ceb6abb5a6f04
peterlil <peterlil@microsoft.com>
2021-02-12 11:10:26 +0100

    Added version

---
+++ sessions/azure-fireworks/azfw-prereqs.ps1
@@ -0,0 +1,10 @@
+﻿param(
+    [string] $ResourceGroup
+)
+# Pre-reqs -> Be logged in to the right subscription first
+
+az group create -g AzureFireworks $ResourceGroup
+
+az appservice plan create -n "ASP-AFW" -g $ResourceGroup -l westeurope --sku S1
+
+az monitor app-insights
